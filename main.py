f13541645db7c0e468f10ca884b09464574e7637
Shivam Panigrahi <73484687+shivam821@users.noreply.github.com>
2025-01-19 15:34:53 +0530

    commit

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
   
-app = Flask(__name__) #creating the Flask class object   
+app = Flask(__name__) 
  
 @app.route('/') 
 @app.route('/landingPage') 
 def landingPage():
     return render_template('main/landingPage.html')
 
-@app.route('/loginPage') 
+@app.route('/loginPage',methods = ['GET','POST']) 
 def loginPage():
+    if request.method == 'POST' : 
+        userEmail = request.form.get('email')
+        userPassword = request.form.get('password')
+        return f'{userEmail} - {userPassword}'
     return render_template('authentication/loginPage.html')
 
-@app.route('/signupPage') 
+@app.route('/signupPage',methods = ['GET','POST']) 
 def signupPage():
+    if request.method == 'POST' : 
+        userEmail = request.form.get('email')
+        userUsername = request.form.get('username')
+        userPassword = request.form.get('password')
+        userCPassword = request.form.get('password')
+        return f'{userEmail} - {userUsername} - {userPassword} - {userCPassword}'
     return render_template('authentication/signupPage.html')
 
 
